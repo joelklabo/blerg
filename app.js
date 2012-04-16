15c7706345271e316d4b861be80574f87553a19b
Joel Klabo <joelklabo@gmail.com>
2012-04-16 14:33:31 -0700

    moving things around

--- app.js
+++ app.js
@@ -6,18 +6,11 @@ var tako = require('tako')
   , port = process.env.DEV_MODE == 'true' ? 8000 : 80
   ;
 
-app.route('/static/*').files(path.join(__dirname, 'static'))
 
-app.route('/')
-  .html(function (req, res) {
-    fs.createReadStream('./static/index.html').pipe(res)
-  })
-  .methods('GET')
+// serve files
+app.route('/*').files(__dirname)
 
-//app.route('/hello.json').json({msg:'hello!'})
-
-//app.route('/plaintext').text('I like text/plain')
 
 app.httpServer.listen(port)
 
-console.log('listening on port: ' + port);
+console.log('listening on port: ' + port)
