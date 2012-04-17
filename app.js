ac9d0a9cd1db1367d3229ec329318d0512487306
Joel Klabo <joelklabo@gmail.com>
2012-04-16 17:02:40 -0700

    removing unused requires

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 var tako = require('tako')
-  , request = require('request')
-  , path = require('path')
   , fs = require('fs')
   , app = tako()
   , port = process.env.DEV_MODE == 'true' ? 8000 : 80
   ;
 
-
-app.route('/').html(function (req, res){
+app.route('/').html(function (req, res) {
   fs.createReadStream('./index.html').pipe(res)
 })
 
 // serve files
 app.route('/*').files(__dirname)
-
 
 app.httpServer.listen(port)
 
