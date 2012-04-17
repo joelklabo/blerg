0fbcd20639b5de64284b247c89a9d7b740a679ee
Joel Klabo <joelklabo@gmail.com>
2012-04-16 23:51:17 -0700

    added tweets

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var tako = require('tako')
   , fs = require('fs')
   , app = tako()
+  , request = require('request')
   , port = process.env.DEV_MODE == 'true' ? 8000 : 80
+  , tweetUrl = 'https://twitter.com/statuses/user_timeline/16474253.json'
   ;
 
 app.route('/').html(function (req, res) {
   fs.createReadStream('./index.html').pipe(res)
+})
+
+app.route('/tweets.json').json(function (req, res) {
+  request(tweetUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.end(body)
+    }
+  })
 })
 
 // serve files
--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-/* Author:
+$(document).ready(function () {
+  $.ajax({
+    url: "/tweets.json" 
+  }).done(function (data) {
+    var data = JSON.parse(data)
+      , str
+      , tweets
+      , html = []
+      ;
 
-*/
-
-
-
-
-
+    for (i = 0; i < data.length; i++) {
+      str = '<div class="tweet-wrap"><p class="tweet">' + data[i].text + '</p></div>';
+      html.push(str)
+    }
+    tweets = $(html.join(''))
+    $('#tweets').append(tweets)
+  })
+})
