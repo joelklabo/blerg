0891371ccf7058ace2f0041db5b3b4c76886f5cf
Joel Klabo <joelklabo@gmail.com>
2012-04-18 22:55:19 -0700

    Moving config to deps

---
+++ deps/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  tweetUrl:     'https://twitter.com/statuses/user_timeline/16474253.json'
+  , githubUrl:  'https://api.github.com/users/joelklabo/events/public'
+  , db:         'http://localhost:5984/actions/'
+}
