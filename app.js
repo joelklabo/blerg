a59eeaf159b4a181093899baa7096cb2796ef945
Joel Klabo <joelklabo@gmail.com>
2012-05-08 20:03:44 -0700

    breaking app into app and server

--- app.js
+++ app.js
@@ -3,32 +3,16 @@ var tako        = require('tako')
   , path        = require('path')
   , request     = require('request')
   , db          = require('./deps/db')
-  , poller      = require('./deps/poller')
   , config      = require('./deps/config')
   , processor   = require('./deps/processors')
   , ghProcess   = new processor.GitHubPreProcessor()
   , twProcess   = new processor.TwitterPreProcessor()
   , postProcess = new processor.PostProcessor()
-  , tenMins     = 60 * 1000 * 10
   , port        = process.env.DEV_MODE == 'true' ? 8000 : 80
   , app         = tako()
   ;
 
 app.actions = []
-
-// Instantiating GitHub Poller
-poller(config.githubUrl, tenMins, function (data) {
-  ghProcess.trans(data).forEach(function (datum) {
-    db.add(datum)
-  })
-})
-
-// Instatiating Twitter Poller
-poller(config.tweetUrl,  tenMins, function (data) {
-  twProcess.trans(data).forEach(function (datum) {
-    db.add(datum)
-  })
-})
 
 // Fill the actions array the first time
 updateActions()
@@ -52,7 +36,6 @@ function updateActions() {
 app.templates.directory(path.resolve(__dirname, 'templates'))
 
 app.route('/').json(function (req, res) {
-  //fs.createReadStream('./index.html').pipe(res)
   function finish(data) {
     db.getAll(function (data) {
       return postProcess.trans(data)
@@ -60,12 +43,6 @@ app.route('/').json(function (req, res) {
   }
   req.pipe(renderPosts(finish)).pipe(res)
 })
- 
-//app.route('/actions.json').json(function (req, res) {
-//  db.getAll(function (data) {
-//    res.end(postProcess.trans(data))
-//  })
-//})
  
 // serve files
 app.route('/*').files(__dirname)
---
+++ server.js
@@ -0,0 +1,27 @@
+var db          = require('./deps/db')
+  , poller      = require('./deps/poller')
+  , config      = require('./deps/config')
+  , processor   = require('./deps/processors')
+  , ghProcess   = new processor.GitHubPreProcessor()
+  , twProcess   = new processor.TwitterPreProcessor()
+  , postProcess = new processor.PostProcessor()
+  , tenMins     = 60 * 1000 * 10
+  ;
+
+// Instantiating GitHub Poller
+poller(config.githubUrl, tenMins, function (data) {
+  console.log('polling for github actions...')
+  ghProcess.trans(data).forEach(function (datum) {
+    db.add(datum)
+  })
+})
+
+// Instatiating Twitter Poller
+poller(config.tweetUrl,  tenMins, function (data) {
+  console.log('polling for tweets...')
+  twProcess.trans(data).forEach(function (datum) {
+    db.add(datum)
+  })
+})
+
+console.log('server started')
