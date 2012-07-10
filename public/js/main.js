ae7ff10053b7b0b13b6d97e6a96eed0227177b26
Joel Klabo <joelklabo@gmail.com>
2012-07-10 01:13:24 -0700

    now commits show up on push events

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,13 @@ $(function () {
       , url = 'https://twitter.com/joelklabo/status/' + id; 
     window.location.replace(url) 
   })
+  $('.github').hover(function () {
+    var $list = $(this).find('ul');
+    $list.fadeIn('slow');
+  }, function () {
+    var $list = $(this).find('ul');
+    $list.fadeOut('slow');
+  })
   $('.item a').click(function (e) {
     e.stopPropagation()
   })
