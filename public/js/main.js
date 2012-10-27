93e700606de10d9c03d5c30a98bc721d82ca7537
Joel Klabo <joelklabo@gmail.com>
2012-10-26 19:58:55 -0700

    WIP

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,24 @@
 $(function () {
 
-  Actions = new Backbone.Collection
+  Actions = Backbone.Collection.extend({
+    events: {
+      'reset': 'reset'
+    }
+    , reset: function () {
+      item = new ActionItem({ model: actions.first()})
+      item.render()
+    }
+  })
+
+  ActionItem = Backbone.View.extend({
+    template: _.template(
+      '<li class="item"><h3>test</h3></li>'
+    )
+    , render: function () {
+      $('.action-list').append(this.template())
+    }
+  })
+  
+  actions = new Actions
 
 })
